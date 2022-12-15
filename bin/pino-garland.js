63e1cfb007d7fcfcf5d99dcc753c72dc6c265fd1
Tycho Tatitscheff <tychot@bam.tech>
2022-12-15 11:56:08 +0100

    refactor: use module for bin

--- bin/pino-garland.js
+++ bin/pino-garland.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
-const pump = require("pump");
-const split = require("split2");
-const { Transform } = require("readable-stream");
+import { fstatSync } from "fs";
+import pump from "pump";
+import split from "split2";
+import { Transform } from "readable-stream";
 
-const { parse, print, printUnknown } = require("../dist/pino-garland.cjs");
+import { parse, print, printUnknown } from "../dist/pino-garland.mjs";
 
 const pretty = (/** @type {any} */ jsonLine) => {
   try {
@@ -32,6 +32,6 @@ pump(process.stdin, split(), prettyTransport, process.stdout);
 function noOp() {}
 
 // https://github.com/pinojs/pino/pull/358
-if (!process.stdin.isTTY && !fs.fstatSync(process.stdin.fd).isFile()) {
+if (!process.stdin.isTTY && !fstatSync(process.stdin.fd).isFile()) {
   process.once("SIGINT", noOp);
 }
