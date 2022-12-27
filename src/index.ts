822542553e760a25fe98007b7039a02b7b108b1e
Tycho Tatitscheff <tychot@bam.tech>
2022-12-27 17:50:43 +0100

    chore: remove useless exports

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,2 @@
 export { parse } from "./parse";
-export {
-  FormatContext,
-  FormatDate,
-  FormatError,
-  FormatHTTP,
-  FormatLevel,
-  FormatMessage,
-  FormatObject,
-  FormatRequestId,
-  FormatSQL,
-} from "./format";
 export { print, printUnknown } from "./print";
